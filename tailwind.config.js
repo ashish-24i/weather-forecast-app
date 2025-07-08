887659df79760f895d499f27e9628fd0bd7a3cc6
ashish-24i <ashishkumar2112000@gmail.com>
2025-07-08 15:19:08 +0530

    Initial commit with project setup and Tailwind files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ content: [
+    "./index.html",
+    "./src/**/*.{html,js}", // Add other extensions if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
